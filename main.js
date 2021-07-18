214a79b5eeedd458a04a0fb978f9339e191f7367
theom95 <theodore.mitchell95@gmail.com>
2021-07-18 13:11:46 -0500

    Got the board to populate with pieces.

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
 const gameBoard = (() => {
     let boardSquares = new Array(9).fill(null);
     const getBoardState = () => boardSquares;
-    const fillSquare = (piece, position) => boardSquares[position] = piece;
+
+    const fillBoardSquare = (event) => {
+        let boardSquare = parseInt(event.target.getAttribute('data-id'));
+        boardSquares[boardSquare] = gameController.getActivePlayer().piece;
+        event.target.innerHTML = gameController.getActivePlayer().piece;
+        gameController.toggleActivePlayer();
+        console.table(boardSquares);
+    }
+
+    const boardGUI = document.querySelectorAll(".ticTacToeBoardSquare");
+    boardGUI.forEach(boardSquare => boardSquare.addEventListener('click', fillBoardSquare));
+
     return {
-        fillSquare,
         getBoardState
     }
 })();
 
 const Player = (piece) => {
-    let isTurn = piece === 'X' ? true : false;
-    return {piece, isTurn}
+    return {piece}
 };
-player1 = Player('X');
-player2 = Player('O');
 
-// const gameController = (() => {
+const gameController = (() => {
+    const player1 = Player('X');
+    const player2 = Player('O');
 
-// })();
+    let activePlayer = player1;
+    const toggleActivePlayer = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    };
 
-gameBoard.fillSquare(player1.piece, 3);
-console.log(gameBoard.getBoardState());
+    const getActivePlayer = () => {return activePlayer};
+
+    const playGameRound = () => {
+        let tempSquareIndex = 0;
+        let gameOver = false;
+        while (gameOver !== true){
+            console.table(gameBoard.getBoardState());
+            console.log("Current piece turn:", activePlayer.piece);
+            console.log("Which square are we filling?")
+        }
+    };
+
+    return {playGameRound, getActivePlayer, toggleActivePlayer}
+})();
+
+// gameController.playGameRound()
